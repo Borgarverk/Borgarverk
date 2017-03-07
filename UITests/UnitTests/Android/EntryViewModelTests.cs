fbe68895f8ff949fbda0460152241fc5c61a2497
fanneykri13 <fanneykri13@ru.is>
2017-03-07 22:57:00 +0000

    UI changes and removed test project

--- UITests/UnitTests/Android/EntryViewModelTests.cs
+++ UITests/UnitTests/Android/EntryViewModelTests.cs
@@ -22,36 +22,36 @@ namespace Borgarverk.UITests
 			app = AppInitializer.StartApp(platform);
 		}
 
-		[Test]
-		public void ValidEntryTrueTest()
-		{
-			EntryViewModel viewModel = new EntryViewModel(new DataService(), new SendService());
-			viewModel.Model.Car = "SomeCar";
-			viewModel.Model.Station = "SomeStation";
-			viewModel.Model.No = "SomeNo";
-			viewModel.Model.RoadArea = "4";
-			viewModel.Model.RoadWidth = "3";
-			viewModel.Model.RoadLength = "3";
-			viewModel.Model.TarQty = "3";
-			viewModel.Model.Rate = "3";
+		//[Test]
+		//public void ValidEntryTrueTest()
+		//{
+		//	EntryViewModel viewModel = new EntryViewModel(new DataService(), new SendService());
+		//	viewModel.Model.Car = "SomeCar";
+		//	viewModel.Model.Station = "SomeStation";
+		//	viewModel.Model.No = "SomeNo";
+		//	viewModel.Model.RoadArea = "4";
+		//	viewModel.Model.RoadWidth = "3";
+		//	viewModel.Model.RoadLength = "3";
+		//	viewModel.Model.TarQty = "3";
+		//	viewModel.Model.Rate = "3";
 
-			Assert.True(viewModel.ValidEntry());
-		}
+		//	Assert.True(viewModel.ValidEntry());
+		//}
 
-		[Test]
-		public void ValidEntryFalseTest()
-		{
-			EntryViewModel viewModel = new EntryViewModel(new DataService(), new SendService());
-			viewModel.Model.Car = "";
-			viewModel.Model.Station = "SomeStation";
-			viewModel.Model.No = "SomeNo";
-			viewModel.Model.RoadArea = "4";
-			viewModel.Model.RoadWidth = "3";
-			viewModel.Model.RoadLength = "3";
-			viewModel.Model.TarQty = "3";
-			viewModel.Model.Rate = "3";
+		//[Test]
+		//public void ValidEntryFalseTest()
+		//{
+		//	EntryViewModel viewModel = new EntryViewModel(new DataService(), new SendService());
+		//	viewModel.Model.Car = "";
+		//	viewModel.Model.Station = "SomeStation";
+		//	viewModel.Model.No = "SomeNo";
+		//	viewModel.Model.RoadArea = "4";
+		//	viewModel.Model.RoadWidth = "3";
+		//	viewModel.Model.RoadLength = "3";
+		//	viewModel.Model.TarQty = "3";
+		//	viewModel.Model.Rate = "3";
 
-			Assert.False(viewModel.ValidEntry());
-		}
+		//	Assert.False(viewModel.ValidEntry());
+		//}
 	}
 }
